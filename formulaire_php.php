1b396c22a641f0c214e030a1e3b83274f8f28d64
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2022-10-15 12:17:54 +0200

    test mail

--- formulaire_php.php
+++ formulaire_php.php
@@ -1,10 +1,52 @@
 <?php
-use PHPMailer-master\PHPMailer-master\PHPMailer;
-use PHPMailer-master\PHPMailer-master\Exception;
+//use PHPMailer-master\PHPMailer-master\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
-require 'path/to/PHPMailer-master/src/Exception.php';
-require 'path/to/PHPMailer-master/src/PHPMailer.php';
-require 'path/to/PHPMailer-master/src/SMTP.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+require 'src/Exception.php';
+namespace MyProject;
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+$mail = new PHPMailer;
+
+//require 'path/to/PHPMailer-master/src/Exception.php';
+//require 'path/to/PHPMailer-master/src/PHPMailer.php';
+//require 'path/to/PHPMailer-master/src/SMTP.php';
+
+$mail = new PHPMailer(True);
+
+$mail->isSMTP(); // Paramétrer le Mailer pour utiliser SMTP 
+$mail->Host = 'mail.votredomaine.com'; // Spécifier le serveur SMTP
+$mail->SMTPAuth = true; // Activer authentication SMTP
+$mail->Username = 'user@votredomaine.com'; // Votre adresse email d'envoi
+$mail->Password = 'secret'; // Le mot de passe de cette adresse email
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Accepter SSL
+$mail->Port = 465;
+
+$mail->setFrom('from@example.com', 'Mailer'); // Personnaliser l'envoyeur
+$mail->addAddress('To1@example.net', 'Karim User'); // Ajouter le destinataire
+$mail->addAddress('To2@example.com'); 
+$mail->addReplyTo('info@example.com', 'Information'); // L'adresse de réponse
+$mail->addCC('cc@example.com');
+$mail->addBCC('bcc@example.com');
+
+$mail->addAttachment('/var/tmp/file.tar.gz'); // Ajouter un attachement
+$mail->addAttachment('/tmp/image.jpg', 'new.jpg'); 
+$mail->isHTML(true); // Paramétrer le format des emails en HTML ou non
+
+$mail->Subject = 'Here is the subject';
+$mail->Body = 'This is the HTML message body';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+	echo 'Erreur, message non envoyé.';
+	echo 'Mailer Error: ' . $mail->ErrorInfo;
+ } else {
+	echo 'Le message a bien été envoyé !';
+ }
 /*
 	********************************************************************************************
 	CONFIGURATION
